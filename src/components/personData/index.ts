d336a8b4c1674568e486ac1b9afbb6951bf9c6a4
Thoma Mumladze <mumladzea4@gmail.com>
2024-09-24 02:01:25 +0400

    component imports

---
+++ src/components/personData/index.ts
@@ -0,0 +1,15 @@
+import Audience from "./audience/Audience";
+import Bio from "./bio/Bio";
+import FullName from "./fullName/FullName";
+import Networking from "./networking/Networking";
+import UserName from "./userName/UserName";
+import UserImage from "./userImage/UserImage";
+
+export const personData = {
+    Audience,
+    Bio,
+    FullName,
+    Networking,
+    UserName,
+    UserImage,
+};
