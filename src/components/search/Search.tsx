d12ad9f392927e650b33357ec05a89f81b3dea2f
Thoma Mumladze <mumladzea4@gmail.com>
2024-09-24 02:01:03 +0400

    user search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,6 +2,10 @@ import "./style.scss";
 import * as Icon from "react-bootstrap-icons";
 
 import { Loading } from "../loading/Loading";
+import Result from "./Result";
+
+import { useEffect, useState } from "react";
+import { gitHubUsers } from "../../api/api";
 
 interface Props {
     isDarkMode: boolean;
@@ -10,6 +14,60 @@ interface Props {
 export const Search = (props: Props) => {
     const { isDarkMode } = props;
 
+    const [userName, setUserName] = useState("");
+    const [userData, setUserData] = useState<[] | null | [{}]>(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState<boolean | null>(null);
+
+    const handleDataRequest = () => {
+        setLoading(true);
+        if (userName === "") {
+            setTimeout(() => {
+                setLoading(false);
+                setError("no result");
+                setUserData(null);
+            }, 550);
+        } else {
+            gitHubUsers(userName)
+                .then((user: any) => {
+                    setTimeout(() => {
+                        setUserData([{ ...user.data }]);
+                        setLoading(false);
+                        setError("");
+                    }, 550);
+                })
+                .catch((err: any) => {
+                    setTimeout(() => {
+                        setLoading(false);
+                        if (err.response && err.response.status === 404) {
+                            setError("User not found");
+                        } else {
+                            setError("An unexpected error occurred");
+                        }
+                        setUserData(null);
+                    }, 550);
+                });
+        }
+    };
+
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.key === "Enter") {
+                handleDataRequest();
+            }
+
+            if (event.key === "Escape") {
+                setUserName("");
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyPress);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+        };
+    }, [handleDataRequest]);
+
     return (
         <div className="search">
             <div
@@ -17,6 +75,7 @@ export const Search = (props: Props) => {
                 style={{
                     backgroundColor: !isDarkMode ? "#FFF" : "#1e2a47",
                     boxShadow: !isDarkMode ? "" : "none",
+                    marginBottom: userData ? "45px" : "0px",
                 }}
             >
                 <Icon.Search width={26} height={26} fill="dodgerblue" style={{ marginLeft: "25px" }} />
@@ -24,10 +83,16 @@ export const Search = (props: Props) => {
                     style={{ color: isDarkMode ? "#FFF" : "#000", marginLeft: "4px" }}
                     type="text"
                     placeholder="Search GitHub username..."
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
                 />
-                <button style={{ backgroundColor: isDarkMode ? "#0061cf" : "" }}>Search</button>
+                <p className="errorBox">{error}</p>
+                <button onClick={handleDataRequest} style={{ backgroundColor: isDarkMode ? "#0061cf" : "" }}>
+                    Search
+                </button>
             </div>
-            <Loading />
+            <div className="loading">{loading ? <Loading /> : null}</div>
+            <Result isDarkMode={isDarkMode} userData={userData} />
         </div>
     );
 };
